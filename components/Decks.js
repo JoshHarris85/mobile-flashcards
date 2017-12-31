f1a8b62624c1d3f61634e9c027dac0eebdc6fa62
JoshHarris85 <joshua.harris1085@gmail.com>
2017-12-31 17:25:56 -0600

    feat: add initial decks view

--- components/Decks.js
+++ components/Decks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 // import { connect } from 'react-redux'
 // import { receiveEntries, addEntry } from '../actions'
 import { fetchDecks } from '../utils/api'
@@ -13,24 +13,50 @@ class Decks extends Component {
   }
 
   render() {
-    const { decks } = this.props
-    console.log(decks.map((deck) => { return deck }))
     return (
-      <View style={styles.center}>
-        <Text>
-          {decks.map((deck) => { return deck.title })}
-        </Text>
+      <View style={styles.container}>
+        <FlatList
+          keyExtractor={item => item.title}
+          data={this.props.decks}
+          renderItem={({item}) =>
+            <TouchableOpacity onPress={() => console.log('clicked!')} >
+              <View style={styles.item}>
+                <Text style={{fontSize: 20}}>
+                  { item.title }
+                </Text>
+                <Text style={styles.item_cards}>
+                  { item.questions.length } { item.questions.length === 1 ? 'card' : 'cards'}
+                </Text>
+                <View style={styles.line}/>
+              </View>
+            </TouchableOpacity>
+          }
+        />
       </View>
-    )
+    );
   }
 }
 
-
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  container: {
+   flex: 1,
+   alignSelf: 'stretch',
+   padding: 30
+  },
+  item: {
+    alignItems: 'center',
+    padding: 25
+  },
+  item_cards: {
+    fontSize: 16,
+    color: '#757575',
+    textAlign: 'center'
+  },
+  line: {
+    paddingTop: 30,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    alignSelf: 'stretch'
   }
 })
 
