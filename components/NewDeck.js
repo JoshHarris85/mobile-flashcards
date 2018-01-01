0f2946cc30d88dbf386982ca32dd37bffab66a4b
JoshHarris85 <joshua.harris1085@gmail.com>
2017-12-31 18:56:57 -0600

    feat: Add create new deck view

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import { connect } from 'react-redux'
 
 class NewDeck extends Component {
@@ -7,8 +7,19 @@ class NewDeck extends Component {
     const { decks } = this.props
 
     return (
-      <View style={styles.center}>
-        <Text>New Deck</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          What is the title of your new deck?
+        </Text>
+        <TextInput
+          style={styles.input}
+          nderlineColorAndroid="black"
+          onChangeText={(text) => console.log(text)}
+          placeholder='Deck Title'
+        />
+        <TouchableOpacity style={styles.button} onPress={() => console.log('clicked!')}>
+          <Text style={{ color: 'white' }}> Submit </Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -16,10 +27,36 @@ class NewDeck extends Component {
 
 
 const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   alignSelf: 'stretch',
+   justifyContent: 'center',
+   padding: 30
+  },
   center: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    padding: 40,
+    textAlign: 'center'
+  },
+  input: {
+    height: 50,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+    textAlign: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    margin: 40,
+    padding: 10,
+    borderRadius: 10
   }
 })
 
